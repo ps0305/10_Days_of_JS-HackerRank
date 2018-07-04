0f5cfe2364b400dfe9c4cad139ca4d589b5d2cfd
Pankaj Kumar Singh <34129569+ps0305@users.noreply.github.com>
2018-07-04 20:08:06 +0530

    completed

---
+++ Day 02/Arithmetic Operators.js
@@ -0,0 +1,17 @@
+  // Calculate the area of a rectangle.
+
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area = length * width;
+    return area;
+}
+
+  // Calculate the perimeter of a rectangle.
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter = 2 * (length + width);
+    return perimeter;
+}
